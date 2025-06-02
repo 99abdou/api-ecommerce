e13eed64fe9df4ace0ba1ab4e5bb74ad9214fe3a
99Abdou <Abdoulayejuniorsantos@gmail.com>
2025-06-02 13:29:53 +0000

    database

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['api-ecommerce-vm8w.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -86,12 +86,7 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+
 
 DATABASES = {
     'default': dj_database_url.parse(config('DATABASE_URL'))
